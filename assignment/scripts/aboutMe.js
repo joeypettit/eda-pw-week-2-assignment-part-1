7ce35b35a173c7c5b12ff9bb7fe8ca2cc0403e43
Joey Pettit <pettitbass@gmail.com>
2022-07-28 14:12:36 -0500

    completed aboutMe.js up through comment 16

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -34,19 +34,38 @@ pets += 2
 console.log(pets)
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+let allowedPets = 3
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous){
+    console.log("Adventures are great!");
+}
+else{
+    console.log("How about we stay home?");
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if(luckyNumber == 2 && adventurous){
+    console.log("Roll the dice!");
+}
+else{
+    console.log("Don't roll the dice!");
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if(pets < allowedPets){
+    console.log("I can have more pets!");
+} if else (pets == allowedPets){
+    console.log("I have enough pets");
+} else{
+    console.log("Oh no, I have too many pets!");
+}
 
 // STRETCH GOALS:
 
@@ -55,6 +74,19 @@ console.log(pets)
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+
+let mostPets
+
+if( pets > friendsPets ){
+    mostPets = pets;
+    console.log("you have the most pets");
+} else if ( pets < friendsPets){
+    mostPets = friendsPets;
+    console.log("your friend has the most pets");
+} else{
+    mostPets = pets;
+    console.log("you each have the same amount of pets")
+}
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
